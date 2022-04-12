4909431e6062da9c7deef9ad629d8b0db49286f5
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-04-11 22:50:29 -0400

    Update kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -1,17 +1,23 @@
 /* Name: Sakib Khan
-Professor: Tong Yi
-Assignment: Kernel
-The operation replaces pixel e (in the middle of the 3x3 window) with some function of its neighbors f(a,b,c,d,e,f,g,h,i)
+//Course: CSCI-135 
+//Instructor: Tong Yi
+//Assignment: Task G. Pixelate,  Lab 8G
+//A sliding window operator replaces each pixel 
+//with some function of its 8 neighbors (and itself).
 */
-
+//Consider pixel e and its 8 neighbors (labeled a-i) 
+//that form a 3x3 window around it.
+//The operation replaces pixel e 
+//(in the middle of the 3x3 window) 
+//with some function of its neighbors 
+//f(a,b,c,d,e,f,g,h,i). It is possible to implement blur,
+// edge detection, and many other image processing operations using this technique.
 #include <iostream>
 #include <cassert>
 #include <cstdlib>
 #include <fstream>
 #include <cstring>
-
 using namespace std;
-
 const int MAX_H = 512;
 const int MAX_W = 512;
 void readImage(int image[MAX_H][MAX_W], int &height, int &width) {
@@ -23,20 +29,73 @@ instr >> c;
 assert(c == 'P');
 instr >> c;
 assert(c == '2');
-
 // skip the comments (if any)
 while ((instr>>ws).peek() == '#') {
-// while loop
 instr.ignore(4096, '\n');
 }
-
 instr >> width;
 instr >> height;
-
 assert(width <= MAX_W);
 assert(height <= MAX_H);
 int max;
 instr >> max;
 assert(max == 255);
-
 for (int row = 0; row < height; row++)
+for (int col = 0; col < width; col++)
+instr >> image[row][col];
+instr.close();
+return;
+}
+void writeImage(int image[MAX_H][MAX_W], int height, int width) {
+ofstream ostr;
+ostr.open("outImage.pgm");
+if (ostr.fail()) {
+cout << "Unable to write file\n";
+exit(1);
+};
+ostr << "P2" << endl;
+// width, height
+ostr << width << ' ';
+ostr << height << endl;
+ostr << 255 << endl;
+for (int row = 0; row < height; row++) {
+for (int col = 0; col < width; col++) {
+assert(image[row][col] < 256);
+assert(image[row][col] >= 0);
+ostr << image[row][col] << ' ';
+ostr << endl;
+}
+}
+ostr.close();
+return;
+}
+int main() {
+int img[MAX_H][MAX_W];
+int H, W;
+readImage(img, H, W); 
+int out[MAX_H][MAX_W];
+int temp[MAX_H+2][MAX_W+2];
+memset(temp,0,sizeof(temp));
+for(int row = 0; row < H; row++) {
+for(int col = 0; col < W; col++) {
+temp[row+1][col+1] = img[row][col];
+}
+}
+int a,b,c,d,e,f,g,h,i;
+int func;
+for(int row =1; row<H+1; row++ ){
+for(int col=1; col<W+1; col++){
+a= temp[row-1][col-1];
+b=temp[row-1][col];
+c=temp[row-1][col+1];
+g=temp[row+1][col-1];
+h=temp[row+1][col];
+i=temp[row+1][col+1];
+func= (g+2*h+i)-(a+2*b+c);
+if(func<0) func=0;
+if(func>255) func=255;
+out[row-1][col-1] = func;
+}
+}
+writeImage(out, H, W);
+}
